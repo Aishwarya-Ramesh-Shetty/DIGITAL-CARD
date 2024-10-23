a9b9c3a3a8b6bdf8b5b9bdee7f2ccc8a6044ebf9
Aishwarya Shetty <shettyaishu050@gmail.com>
2024-10-23 17:22:24 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,17 +1,1 @@
-import React from "react";
 
-
-
-const Footer = () =>{
-    return(
-        <div>
-            <div>
-                <img onScroll={"./Images/aishuuu.jpeg"} />
-            </div>
-
-
-        </div>
-    );
-};
-
-export default Footer;
